5d5c3e587da9f9c23b10bce98ece4d15fa27d34b
Fam Awad <fam@awadlouis.com>
2024-05-18 12:41:14 +0200

    Initial commit

--- Main.asm
+++ Main.asm
@@ -1,1 +1,25 @@
+org 100h
 
+.data SEGMENT 
+    ROWS EQU 4
+    COLS EQU 4
+    key DB ROWS * COLS DUP(?)
+
+.code SEGMENT 
+
+; -------------------- MACROS --------------------
+
+; MACRO: shiftRows
+; DESCRIPTION: Shifts the rows of the key matrix to the left by the specified number of positions
+shiftRows MACRO
+
+ENDM
+
+shiftRowsHelper MACRO row, shiftAmount
+    MOV CX, shiftAmount
+    Loop: 
+        PUSH key[row][CX]
+        LOOP Loop
+
+ENDM
+    
