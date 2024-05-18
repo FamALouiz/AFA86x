0654edc91f9a7027e7ea1784b0b69833e8b1f5d9
Fam Awad <fam@awadlouis.com>
2024-05-18 12:43:24 +0200

    Updated branch and merged main

--- Main.asm
+++ Main.asm
@@ -1,12 +1,34 @@
+; Title: AES 8086
+; Author: AFA86x
+; Date: 18/5/2024
 
 org 100h
 
-.data SEGMENT 
+; Define constants, variables, and data sections
+section .data
+    ; Define your data variables here
     ROWS EQU 4
     COLS EQU 4
     key DB ROWS * COLS DUP(?)
+section .bss
+    ; Define uninitialized data variables here
 
-.code SEGMENT 
+; Define code section
+section .text
+    ; Declare global entry point
+    global _start
+
+; Entry point
+_start:
+    ; Your code starts here
+
+    ; Example: Exit program
+    mov eax, 1       ; System call number for exit
+    xor ebx, ebx     ; Exit code 0
+    int 0x80         ; Invoke system call
+
+; Define additional functions or code sections below if needed
+
 
 ; -------------------- MACROS --------------------
 
@@ -23,4 +45,3 @@ shiftRowsHelper MACRO row, shiftAmount
         LOOP Loop
 
 ENDM
-    
