1a90d4063efc92d373bebdf99e1fe6423f7a8718
Fam Awad <fam@awadlouis.com>
2024-05-19 17:52:02 +0200

    Added shiftElementsToTheRight Macro + updated shiftRowsHelper logic

--- Main.asm
+++ Main.asm
@@ -23,13 +23,31 @@ shiftRows MACRO
 ENDM
 
 shiftRowsHelper MACRO row, shiftAmount
+    
+
     MOV CX, shiftAmount - 1
-    PUSH_LOOP_START: 
+    PUSH_LOOP_START:     ; Push the elements of the row to the stack
         PUSH key[row][CX]
         LOOP PUSH_LOOP_START
+    
+    MOV DX, shiftAmount ; Use DX since CX will be used in the next loop
+    SHIFT_LOOP_START: 
+        shiftElementsToTheRight row
+        SUB DX, 1
+        JNE SHIFT_LOOP_START
 
     MOV CX, shiftAmount
-    POP_LOOP_START: 
+    POP_LOOP_START: ; Pop the elements from the stack to the row
         POP key[row][4 - CX]
         LOOP POP_LOOP_START
+ENDM
+
+
+shiftElementsToTheRight MACRO row
+    MOV CX, 0
+    SWAP_ITEMS_LOOP: ; Shifts elements to the right
+        XCHG key[row][CX], key[row][CX + 1]
+        ADD CX, 1
+        CMP CX, 4
+        JNE SWAP_ITEMS_LOOP
 ENDM
