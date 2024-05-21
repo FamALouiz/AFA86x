a25c57386203f08e7cf98a8a9f58cc627ca5c9a8
Fam Awad <fam@awadlouis.com>
2024-05-21 22:14:16 +0200

    Added addRoundKeyMacros

--- Main.asm
+++ Main.asm
@@ -10,8 +10,10 @@ org 100h
     ROWS EQU 4
     COLS EQU 4  ;0  1  2  3  4  5  6  7  8  9   10  11  12  13  14  15
     keyMatrix DB 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+    roundKey DB 0a3h, 012h, 0bdh, 0c2h, 045h, 06fh, 08ah, 07fh, 037h, 076h, 03bh, 09fh, 01fh, 01bh, 06bh, 014h ; Random round key
     include ShiftRowsMacros.inc
+    include AddRoundKeyMacros.inc
 
 ; Define code section
 .code segment 
-    shiftRows
+    addRoundKey
